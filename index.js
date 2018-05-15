1239e3490ccdf5b32ed3b11f88c773c3667d3fcf
sohoorc <me@gwtcoder.me>
2018-05-15 21:11:49 +0800

    upload

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 module.exports = require('./dist/index');
+// "build": "cross-env NODE_ENV=production webpack --config webpack.config.js --mode=production"
+// "build": "webpack --config webpack.config.js"
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: [
-    "babel-polyfill",
     "./src"
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: "index.js", // string
-    publicPath: '/dist/'
+    publicPath: '/dist/',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use:["style-loader","css-loader"] 
+        // use: ExtractTextPlugin.extract({
+        //   fallback: "style-loader",
+        //   use: "css-loader"
+        // })
       },
-      { 
-        test:  /\.(js|jsx|mjs)$/, 
-        exclude: /node_modules/, 
-        loader: "babel-loader"
+      {
+        test: /\.(js|jsx|mjs)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: true
+        }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
@@ -32,4 +37,9 @@ module.exports = {
       }
     ],
   },
+  // plugins:[
+  //   new ExtractTextPlugin({
+  //     filename: '/style/style.css',
+  //   }),
+  // ]
 };
